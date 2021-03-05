c4d07b214070d55c57c02d13240ad063a5f5c890
Baptiste LECHAT <baptistelechat@outlook.fr>
2021-03-05 17:19:20 +0100

    wip(front): make GoHome and GoBack responsive / add Validator to Textfield into modals / add the actual selected profil on top of table in settings

--- Front/src/components/button/floatingButton/ButtonGoHome.js
+++ Front/src/components/button/floatingButton/ButtonGoHome.js
@@ -9,10 +9,16 @@ import HomeIcon from '@material-ui/icons/Home';
 const useStyles = makeStyles((theme) => ({
   btn_home: {
     position: 'fixed',
-    bottom: theme.spacing(2),
-    right: theme.spacing(2),
-    zIndex: 100
-  },
+    bottom: theme.spacing(1),
+    right: theme.spacing(1),
+    zIndex: 100,
+    [theme.breakpoints.up('sm')]: {
+      position: 'fixed',
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+      zIndex: 100,
+    },
+  }
 }));
 
 const ButtonGoHome = () => {
